98d8f616cf0a02c924ce62b32d544ce2a8a63bb0
Jerry Bruwes <jbruwes@icloud.com>
2026-01-14 14:32:47 +0200

    feat(schemas): add credential schema

---
+++ src/schemas/credential.ts
@@ -0,0 +1,13 @@
+import type { JSONSchema } from "json-schema-to-ts";
+
+export default {
+  $id: "credential",
+  properties: {
+    accessKeyId: { default: null, nullable: true, type: "string" },
+    Bucket: { default: null, nullable: true, type: "string" },
+    endpoint: { default: null, nullable: true, type: "string" },
+    region: { default: null, nullable: true, type: "string" },
+    secretAccessKey: { default: null, nullable: true, type: "string" },
+  },
+  type: "object",
+} as const satisfies JSONSchema;
