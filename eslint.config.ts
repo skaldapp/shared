69f1780435056639c05d2e27307dba33877497ec
Jerry Bruwes <jbruwes@icloud.com>
2026-04-07 18:02:56 +0200

    refactor: simplify eslint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,1 @@
-import shared from "@skaldapp/configs/eslint";
-import { defineConfig } from "eslint/config";
-
-const allowDefaultProject = ["*.config.ts"],
-  projectService = { allowDefaultProject },
-  tsconfigRootDir = import.meta.dirname,
-  parserOptions = { projectService, tsconfigRootDir },
-  languageOptions = { parserOptions };
-
-export default defineConfig(shared, { languageOptions });
+export { default } from "@skaldapp/configs/eslint";
